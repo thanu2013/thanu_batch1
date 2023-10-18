e614fea7df9a41353c2f0204f8bf21a25c8f1841
thanu2013 <129086034+thanu2013@users.noreply.github.com>
2023-10-18 11:14:19 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld!!!!!!!"); 
+        System.out.println("HelloWorld****************"); 
     }
 }
